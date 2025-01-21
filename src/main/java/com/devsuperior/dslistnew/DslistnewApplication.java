80cdbefe201b1fd093ce9cf647475bda143c2195
Diego Fonseca <diegoo.fonsecaa@gmail.com>
2025-01-20 21:18:29 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslistnew/DslistnewApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslistnew/DslistnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
