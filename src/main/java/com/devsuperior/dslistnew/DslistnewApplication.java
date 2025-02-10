4c0471bb5ff2adcedcb19554da076a6428cfa86c
Diego Fonseca <diegoo.fonsecaa@gmail.com>
2025-02-09 22:24:20 -0300

    implement GameList feature with controller, service, repository, and DTO

--- src/main/java/com/devsuperior/dslistnew/DslistnewApplication.java
+++ src/main/java/com/devsuperior/dslistnew/DslistnewApplication.java
@@ -3,11 +3,15 @@ package com.devsuperior.dslistnew;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.dslistnew.entities.Game;
+
 @SpringBootApplication
 public class DslistnewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DslistnewApplication.class, args);
 	}
+
+	
 
 }
